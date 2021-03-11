a289f3a0f55cfd34358c3451eeb7b1d71e18c5fd
NQWsLtAuT52r4j4gshk5F2 <80122317+NQWsLtAuT52r4j4gshk5F2@users.noreply.github.com>
2021-03-11 22:17:48 +0100

    feat: integrate Hibernate

---
+++ web/src/web/service/model/User.java
@@ -0,0 +1,39 @@
+package web.service.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+	@Id
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "NAME")
+	private String name;
+
+	public User() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public User setId(Long id) {
+		this.id = id;
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public User setName(String name) {
+		this.name = name;
+		return this;
+	}
+}
--- web/src/web/service/rest/Test.java
+++ web/src/web/service/rest/Test.java
@@ -1,22 +1,27 @@
 package web.service.rest;
 
 import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import web.service.response.Status;
+import web.service.model.User;
 
 @RequestScoped
 @Path("/test")
 @Produces("application/json")
 @Consumes("application/json")
 public class Test {
+	@PersistenceContext
+	EntityManager em;
 
 	@GET
 	public Response test() {
-		return Response.ok(new Status("OK", 200)).build();
+		User user = em.find(User.class, 0L);
+		return Response.ok(user).build();
 	}
 }
